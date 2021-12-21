e79910c2072c961b43269d0e74145a492112b2f8
yuyenews <yuyemail@163.com>
2021-12-21 19:23:30 +0800

    Support for receiving request parameters with function arguments

--- application/web/route/Route.go
+++ application/web/route/Route.go
@@ -16,37 +16,37 @@ func GetRoute(path string) interface{} {
 	return routeMap[path]
 }
 
-// GET Add a wroute for GET request method
+// GET Add a route for GET request method
 func GET(path string, function interface{}) {
 	AddRoute(path, "GET", function)
 }
 
-// POST Add a wroute for POST request method
+// POST Add a route for POST request method
 func POST(path string, function interface{}) {
 	AddRoute(path, "POST", function)
 }
 
-// PUT Add a wroute for PUT request method
+// PUT Add a route for PUT request method
 func PUT(path string, function interface{}) {
 	AddRoute(path, "PUT", function)
 }
 
-// DELETE Add a wroute for DELETE request method
+// DELETE Add a route for DELETE request method
 func DELETE(path string, function interface{}) {
 	AddRoute(path, "DELETE", function)
 }
 
-// PATCH Add a wroute for PATCH request method
+// PATCH Add a route for PATCH request method
 func PATCH(path string, function interface{}) {
 	AddRoute(path, "PATCH", function)
 }
 
-// HEAD Add a wroute for HEAD request method
+// HEAD Add a route for HEAD request method
 func HEAD(path string, function interface{}) {
 	AddRoute(path, "HEAD", function)
 }
 
-// OPTIONS Add a wroute for OPTIONS request method
+// OPTIONS Add a route for OPTIONS request method
 func OPTIONS(path string, function interface{}) {
 	AddRoute(path, "OPTIONS", function)
 }
@@ -62,7 +62,7 @@ func Any(path string, function interface{}) {
 	OPTIONS(path, function)
 }
 
-// AddRoute Add a wroute, and if you need to use another request method, you can use this
+// AddRoute Add a route, and if you need to use another request method, you can use this
 func AddRoute(path string, method string, function interface{}) {
 	routeMap[path+"/"+method] = function
 }
