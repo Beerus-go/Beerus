a94fec257eedbef8e3d0885cf1c2e390883e7322
yuyenews <yuyemail@163.com>
2022-01-23 15:45:19 +0800

    Adding microservice functionality

---
+++ application/cloud/BeerusCloud.go
@@ -0,0 +1,13 @@
+package cloud
+
+func RequestJson(serverName string, url string, param interface{}) (string, error) {
+	return "", nil
+}
+
+func RequestStream(serverName string, url string, param interface{}) ([]byte, error) {
+	return nil, nil
+}
+
+func RequestUpload(serverName string, url string, files [][]byte) (string, error) {
+	return "", nil
+}
